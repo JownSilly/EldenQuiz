a7a1903974d9a280e0d6e8a0cd11c94eb91aa462
JownSilly <joaovitordealmeidaojeda2@hotmail.com>
2023-04-18 18:42:22 -0400

    Troca de Sprite e Desabilitar botoes
    desabilitar botoes e troca de sprite

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -10,7 +10,7 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject[] alternativaTMP;
     [Header ("sprites")]
     [SerializeField] private Sprite spritealtCorreta;
-    [SerializeField] private Sprite spritealtIcorreta;
+    [SerializeField] private Sprite spritealtIncorreta;
     
     // Start is called before the first frame update
     void Start()
@@ -23,16 +23,40 @@ public class GameManager : MonoBehaviour
             alt.SetText(alternativas[i]);
         }
     }
+    //Manuseia as opçoes selecionadas
     public void HandleOption(int alternativaSelecionada)
     {
         if(alternativaSelecionada == perguntaAtual.getRespostaCorreta()){
+            ChangeButtonSprite(alternativaTMP[alternativaSelecionada].GetComponent<Image>(), spritealtCorreta);
+            DisableOptionButtons(alternativaSelecionada);
             Debug.Log("ganhoooo");
         }else{
+            ChangeButtonSprite(alternativaTMP[alternativaSelecionada].GetComponent<Image>(), spritealtIncorreta);
             Debug.Log("perdeuuuuuuu");
+            DisableOptionButtons(alternativaSelecionada);
+            ChangeButtonSprite(alternativaTMP[perguntaAtual.getRespostaCorreta()].GetComponent<Image>(), spritealtCorreta);
         }
     }
+    //Funçao para alterar o sprite dos botoes selecionados
     public void ChangeButtonSprite(Image imgBtn, Sprite spriteAlt){
         imgBtn.sprite = spriteAlt;
+    }
+    //Função para Desabilitar os Botoes desnecessários
+    public void DisableOptionButtons(int alternativaSelecionada)
+    {
+        for(int i= 0; i< alternativaTMP.Length; i++)
+        {
+            Button btn = alternativaTMP[i].GetComponent<Button>();
+            if(alternativaSelecionada != i)
+            {
+                btn.interactable = false;
+            }
+            else
+            {
+                btn.enabled = false;
+            }
+            
+        }
     }
     // Update is called once per frame
     void Update()
